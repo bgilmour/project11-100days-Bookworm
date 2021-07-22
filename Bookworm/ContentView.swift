c5f75563cc6dd330609d349f3244bfe9576ea9f7
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-22 10:14:30 +0100

    Creating a custom component with @Binding

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,6 +9,55 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+
+    var body: some View {
+        PushButtonView()
+    }
+}
+
+struct PushButtonView: View {
+    @State private var rememberMe = false
+
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct RememberMeView: View {
+    @State private var rememberMe = false
+
+    var body: some View {
+        Toggle(isOn: $rememberMe.animation()) {
+            Text("Remember me?")
+        }
+        if rememberMe {
+            Text("Boo!")
+        }
+    }
+}
+
+struct CoreDataContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
